3e2f63cc9fe344edc2b2829e522f2d467c294a9b
Alessiochiesa <91635097+Alessiochiesa@users.noreply.github.com>
2021-11-11 11:29:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+
+    int n=0, x1=0, x2=1, xf=x1+x2;
+   
+   cin>>n;
+  
+   cout<<"1"<<endl;
+   
+   cout<<x2<<endl;
+   
+   for(int i=3; i<=n; i++){
+        cout<<xf<<endl;
+        x1=x2;
+        x2=xf;
+        xf=x1+x2;
+    }
+    return 0;
 }
