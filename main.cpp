960365640ff2e55c01d2163805e93d1e041a4b5c
Alessiochiesa <91635097+Alessiochiesa@users.noreply.github.com>
2021-11-14 22:13:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,25 @@
 
 using namespace std;
 
-int main()
-{
-    int n,i=1,b=1,a=0,c;
+int main() {
+    
+    int n=0;
     cin>>n;
-
-    if(n>=2)
-    {
-        cout<<b<<endl;
-        for(i=2;i<=n;i++)
-        {
-         c=a+b;
-         cout<<c<<endl;
-         a=b;
-         b=c;
-
-        }
-
-
-    }
-    else
-    {
+    
+    if (n<2){
         cout<<"errore"<<endl;
+        
+    }else{
+        cout<<"1"<<endl;
+        int a=0, b=1, c=0;
+        for (int i=0; i<=(n-2); i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+        }
     }
 
 
     return 0;
+}
