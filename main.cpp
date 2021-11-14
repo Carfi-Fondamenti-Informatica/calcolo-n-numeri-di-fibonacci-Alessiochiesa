61aadddfb33b3c316debe7696d4b487c8d2394ff
Alessiochiesa <91635097+Alessiochiesa@users.noreply.github.com>
2021-11-14 22:29:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-   int n=0,b=1,a=0,c=0;
+int main(){
+   int n,i=1,b=1,a=0,c;
    cin>>n;
    
-   cout<<b<<endl;
-   for(c=0;c<n;c++)
+   if(n>=2)
    {
-      c=a+b;
-      cout<<c<<endl;
-      a=b;
-      b=c;
+      cout<<b<<endl;
+      for(i=2;i<=n;i++)
+      {
+         c=a+b;
+         cout<<c<<endl;
+         a=b;
+         b=c;
+      }
+      
    }
+else
+   {
+   cout<<"errore"<<endl;
+   }
+   
    
    return 0;
 }
